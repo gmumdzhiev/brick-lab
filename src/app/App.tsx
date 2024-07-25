d3f40c02c1244c985177cf28d04a63175966aa3d
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2024-07-25 13:54:03 +0200

    creating store and refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
         Hey
@@ -9,4 +9,3 @@ function App() {
   );
 }
 
-export default App;
